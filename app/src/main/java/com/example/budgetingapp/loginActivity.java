24993e4075bf7b3293421af2fe4a7176b21c7ca0
yui <lizardojohnsonc@gmail.com>
2022-09-26 10:07:24 +0800

    with table at top

--- app/src/main/java/com/example/budgetingapp/loginActivity.java
+++ app/src/main/java/com/example/budgetingapp/loginActivity.java
@@ -1,7 +1,9 @@
 package com.example.budgetingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class loginActivity extends AppCompatActivity {
     private EditText email, password;
     private TextView registerButton;
     private Button loginButton;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +32,44 @@ public class loginActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         registerButton = findViewById(R.id.registerButton);
         loginButton = findViewById(R.id.loginButton);
+        mAuth = FirebaseAuth.getInstance();
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(loginActivity.this,registrationActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailString = email.getText().toString().trim();
+                String passwordString = password.getText().toString().trim();
+
+                if(emailString.isEmpty()){
+                    email.setError("please enter email");
+                    email.requestFocus();
+                    return;
+                }
+                if(passwordString.isEmpty()){
+                    password.setError("please Enter Password");
+                    password.requestFocus();
+                    return;
+                }
+                else{
+                    mAuth.signInWithEmailAndPassword(emailString,passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(loginActivity.this,MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                }
             }
         });
 
--- app/src/main/java/com/example/budgetingapp/registrationActivity.java
+++ app/src/main/java/com/example/budgetingapp/registrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.budgetingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,16 +9,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registrationActivity extends AppCompatActivity {
     private EditText email, password;
     private TextView loginTextview;
     private Button registerButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
@@ -29,6 +38,38 @@ public class registrationActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(registrationActivity.this,loginActivity.class);
                 startActivity(intent);
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailString = email.getText().toString().trim();
+                String passwordString = password.getText().toString().trim();
+
+                if(emailString.isEmpty()){
+                    email.setError("Please input email");
+                    email.requestFocus();
+
+                }
+                if(passwordString.isEmpty()){
+                    password.setError("please input password");
+                    password.requestFocus();
+
+                }else{
+                    mAuth.createUserWithEmailAndPassword(emailString,passwordString).addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            Toast.makeText(registrationActivity.this,"success registration",Toast.LENGTH_LONG);
+                            Intent intent = new Intent(registrationActivity.this,loginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(registrationActivity.this, task.getException().toString(),Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+                }
+
+
             }
         });
     }
