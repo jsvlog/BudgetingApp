a3f6af5531af8133ba97614640fb9b535c2ad95e
COA PC <lizardojohnsonc@gmail.com>
2022-09-29 10:31:08 +0800

    office commit

---
+++ app/src/main/java/com/example/budgetingapp/BudgetActivity.java
@@ -0,0 +1,19 @@
+package com.example.budgetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class BudgetActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget);
+
+        fab =
+    }
+}
--- app/src/main/java/com/example/budgetingapp/MainActivity.java
+++ app/src/main/java/com/example/budgetingapp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.budgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private CardView budget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        budget = findViewById(R.id.budgetCardview);
+
+        budget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
