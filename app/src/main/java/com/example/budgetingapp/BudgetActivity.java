e53cf1e394be7991962db358236b41f85217108b
COA PC <lizardojohnsonc@gmail.com>
2022-09-29 13:45:34 +0800

    craeting budget acitivity

--- app/src/main/java/com/example/budgetingapp/BudgetActivity.java
+++ app/src/main/java/com/example/budgetingapp/BudgetActivity.java
@@ -1,8 +1,11 @@
 package com.example.budgetingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -14,6 +17,25 @@ public class BudgetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
 
-        fab =
+        fab = findViewById(R.id.fab);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                additem();
+            }
+        });
+    }
+
+    private void additem() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View myView = inflater.inflate(R.layout.input_layout, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
